cced09240f974abe6b8d2f8aa7b2ede1e57d2797
preslavb <preslavnpetrov@gmail.com>
2019-02-11 21:48:25 +0000

    start moving some of the files around

--- Assets/Scripts/UI/Map/HungerUIUpdater.cs
+++ Assets/Scripts/UI/Map/HungerUIUpdater.cs
@@ -13,6 +13,7 @@ public class HungerUIUpdater : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+        // TODO: Remove this from the update method, and link it up to an event
 		gameObject.GetComponent<Slider>().value = StaticVariables.petData.hunger;
 	}
 }
