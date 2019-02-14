41cef296ee0384628d76ab00bb57b40c910088ce
Scott <lolk>
2019-02-14 14:06:25 +0000

    Cleanup
    Cleanup of most folders and scripts to be placed in correct areas, unused items removed etc

---
+++ Assets/Scene_Combat/Scripts/StaticVariables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticVariables
+{
+	public static PetCombatScript combatPet;
+	public static Material defaultEnemyMaterial;
+	public static Material defaultPetMaterial;
+	public static Material damagedMaterial;
+	public static TraitManager traitManager;
+	public static TraitRendererScript traitRenderer;
+	public static bool isInBattle;
+
+	public static System.Random RandomInstance = new System.Random();
+
+	public static List<EnemyComponent> EnemyComponents = new List<EnemyComponent>();
+	public static BattleInputHandler battleHandler;
+	public static UIHandler uiHandler;
+	public static PersistanceManagerScript persistanceManager;
+	public static SceneTransitionHandler sceneManager;
+	public static PetAI petAI;
+	public static GameObject map;
+	public static PetData petData;
+	public static PlayerData playerData;
+	public static int BombsInPlay { get; set; }
+
+	public delegate void AttackDelegate();
+
+	public static event AttackDelegate AttackCallbacks;
+
+	public static void ResolveAttacks()
+	{
+		AttackCallbacks?.Invoke();
+
+		AttackCallbacks = null;
+	}
+}
