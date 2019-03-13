3e34e4f0ecc8a0c07baaf75ebd1a84944ead06c4
preslavb <preslavnpetrov@gmail.com>
2019-03-13 01:02:41 +0000

    catalysts almost reimplemented

---
+++ Assets/Scene_Combat/Trait Scripts/CatalystEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatalystEffect : GenericEffect {
+
+    // The rarities supported by the effect
+    public bool[] supportedRarities = new bool[] { true, true, true, true, true };
+
+    // The rarity of the instantiated catalyst effect
+    private Rarities _rarity;
+
+    public Rarities rarity
+    {
+        get
+        {
+            return _rarity;
+        }
+
+        set
+        {
+            if (supportedRarities[(int)value])
+            {
+                _rarity = value;
+            }
+            else
+            {
+                _rarity = Rarities.Unsupported;
+            }
+        }
+    }
+}
